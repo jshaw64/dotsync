463b1e84d7182f228845eb9ce6fc8260826ab8ae
jshaw <jshaw64@gmail.com>
2016-07-17 14:47:16 -0400

    Remove old test

--- sync.sh
+++ sync.sh
@@ -4,33 +4,6 @@ E_FSRC=50
 E_DROOT=51
 E_FDST=52
 
-sync_test()
-{
-	. ./config.sh
-
-	echo "Hello-----"
-
-	local darchive="${HOME}/Documents/test-data/dotarchive"
-
-	local i=0
-	local size="${#TEST_CONFIG_VALS[@]}"
-	while [ $i -lt $size ]; do
-		config_test_parse $i
-		local fsrc=$(config_get "$CONF_KEY_FSRC")
-		local fdst=$(config_get "$CONF_KEY_FDST")
-		local droot=$(config_get "$CONF_KEY_DROOT")
-		local dgroup=$(config_get "$CONF_KEY_DGROUP")
-		validate_before "$fsrc" "$droot"
-		[ $? -gt 0 ] && exit $?
-		do_archive "$fsrc" "$darchive"
-		do_link "$fsrc" "$fdst" "$droot" "$dgroup"
-		validate_after "$fsrc" "$fdst" "$droot" "$dgroup" "$darchive"
-		(( i++ ))
-	done
-	echo "Hello-----"
-}
-
-
 do_archive()
 {
 	local fsrc="$1"
