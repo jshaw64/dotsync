5ae3598140f307ba3402fe217cd5b0bb9fbb1dbe
jshaw <jshaw64@gmail.com>
2016-07-17 14:52:37 -0400

    Move entry point outside sync
    Necessary in order to reuse sync components, and run tests against those
    components.

---
+++ run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+main()
+{
+    local darchive="${HOME}/dotarchive"
+
+    local i=0
+    local size="${#conf_static[@]}"
+    while [ $i -lt $size ]; do
+        config_parse $i
+        local fsrc=$(config_get "$CONF_KEY_FSRC")
+        local fdst=$(config_get "$CONF_KEY_FDST")
+        local droot=$(config_get "$CONF_KEY_DROOT")
+        local dgroup=$(config_get "$CONF_KEY_DGROUP")
+        validate_before "$fsrc" "$droot"
+        [ $? -gt 0 ] && exit $?
+        do_archive "$fsrc" "$darchive"
+        do_link "$fsrc" "$fdst" "$droot" "$dgroup"
+        validate_after "$fsrc" "$fdst" "$droot" "$dgroup" "$darchive"
+        (( i++ ))
+    done
+}
+
+init()
+{
+    local ctx="${BASH_SOURCE%/*}"
+    if [[ ! -d "$ctx" ]]; then ctx="$PWD"; fi
+    . "$ctx/config.sh"
+    . "$ctx/sync.sh"
+}
+
+init
+main "$@"
--- sync.sh
+++ sync.sh
@@ -78,32 +78,3 @@ validate_before()
 
 	return 0
 }
-
-main()
-{
-
-    local ctx="${BASH_SOURCE%/*}"
-    if [[ ! -d "$ctx" ]]; then ctx="$PWD"; fi
-    . "$ctx/config.sh"
-
-	local darchive="${HOME}/dotarchive"
-
-	local i=0
-	local size="${#conf_static[@]}"
-	while [ $i -lt $size ]; do
-		config_parse $i
-		local fsrc=$(config_get "$CONF_KEY_FSRC")
-		local fdst=$(config_get "$CONF_KEY_FDST")
-		local droot=$(config_get "$CONF_KEY_DROOT")
-		local dgroup=$(config_get "$CONF_KEY_DGROUP")
-		validate_before "$fsrc" "$droot"
-		[ $? -gt 0 ] && exit $?
-		do_archive "$fsrc" "$darchive"
-		do_link "$fsrc" "$fdst" "$droot" "$dgroup"
-		validate_after "$fsrc" "$fdst" "$droot" "$dgroup" "$darchive"
-		(( i++ ))
-	done
-}
-
-main "$@"
-
